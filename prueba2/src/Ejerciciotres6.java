cfa93164a571aa2953b4efe630b6e8c9582210b8
Daniel <danyextr@gmail.com>
2023-02-07 00:00:23 +0100

    no me entero de mierda de prmo. ya mañana miro más...

---
+++ prueba2/src/Ejerciciotres6.java
@@ -0,0 +1,23 @@
+public class Ejerciciotres6 {
+
+    public static void main(String[] args){
+
+        int n;
+        boolean primo;
+        int prim;
+        int primcum=0;
+
+
+
+            System.out.println("Introduce un número: ");
+            n=Entrada.entero();
+
+            for(int i=1;i<=n;i++){
+
+                
+            }
+
+
+
+    }
+}
