cac0b3dc617852c6f5155b6bc1bb8d0a69e84d8f
Daniel <danyextr@gmail.com>
2023-02-06 23:49:20 +0100

    este ha sido difícil, pero más o menos...

--- prueba2/src/Ejerciciotres4.java
+++ prueba2/src/Ejerciciotres4.java
@@ -2,6 +2,20 @@ public class Ejerciciotres4 {
 
     public static void main(String[] args){
 
-        
+        int n;
+        int fila, col;
+
+        System.out.println("Lado del cuadrado: ");
+        n=Entrada.entero();
+
+        for(fila=1; fila<=n; fila++){
+
+            for(col=1; col<=n; col++)
+
+
+                System.out.print("* ");
+                System.out.println("");
+
+        }
     }
 }
---
+++ prueba2/src/Ejerciciotres5.java
@@ -0,0 +1,49 @@
+public class Ejerciciotres5 {
+
+    public static void main(String[] args){
+
+        for(int i=0; i<10;i++){
+            for(int j=0; j<10; j++){
+                for(int k=0;k<10;k++){
+                    for(int m=0;m<10;m++){
+                        for(int n=0;n<10;n++){
+
+                            if(i==3)
+                                System.out.print("E");
+                                else
+                                    System.out.print(i);
+
+
+                            if(j==3)
+                                System.out.print("E");
+                                else
+                                    System.out.print(j);
+
+
+                            if(k==3)
+                                System.out.print("E");
+                                else
+                                    System.out.print(k);
+
+
+                            if(m==3)
+                                System.out.print("E");
+                                else
+                                    System.out.print(m);
+
+
+                            if(n==3)
+                                System.out.print("E");
+                                else
+                                    System.out.print(n);
+
+
+                            System.out.println(" ");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
