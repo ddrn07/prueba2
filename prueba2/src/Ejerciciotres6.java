5b3db8bed7329ecc22e77a95281bf9868afcd4e5
Daniel Rodríguez <danyextr@gmail.com>
2023-02-07 14:08:46 +0100

    Me ha tenido que explicar Luis lo del primo, y aun así no está muy claro...

--- prueba2/src/Ejerciciotres6.java
+++ prueba2/src/Ejerciciotres6.java
@@ -2,22 +2,34 @@ public class Ejerciciotres6 {
 
     public static void main(String[] args){
 
-        int n;
+
+        int j, num, cont_pri;
         boolean primo;
-        int prim;
-        int primcum=0;
 
+        cont_pri=0;
+        System.out.println("Introduce número: ");
+        num=Entrada.entero();
 
+        for(int i=1;i<=num;i++){
 
-            System.out.println("Introduce un número: ");
-            n=Entrada.entero();
+            primo=true;
+            j=2;
+            while(j<=i-1 && primo==true)
+            {
 
-            for(int i=1;i<=n;i++){
-
-                
+                if(i%j==0)
+                    primo=false;
+                j++;
             }
 
+            if(primo==true){
 
+                cont_pri++;
+                System.out.println(i +(" es primo"));
+            }
+        }
+
+        System.out.println("En el rango 1.." + num + ", hay " +cont_pri + " números primos.");
 
     }
 }
